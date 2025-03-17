25b410ece062056ad618c94c411187c7606b90b7
markbozsoki <bozsoki.mark@icloud.com>
2025-03-17 23:10:16 +0100

    load_table_data_from_js

--- resources/2/load_table.js
+++ resources/2/load_table.js
@@ -1,7 +1,17 @@
+let data_table = document.getElementById("dataTable");
+data_table.onload = loadTableData();
 
-
-
-
+function loadTableData() {
+    let table_data = [
+        ["Lee Sang-hyeok", "lee.shang@gmail.com", "@Faker", "12000"],
+        ["Johan Sundstein", "stndsteing@icloud.com", "@N0tail", "11000"],
+        ["Sebastien Dabs", "dabs.rules@freemail.com", "@Ceb", "10000"],
+        ["Kuro Takhasomi", "takhasomi@anymail.uk", "@KuroKy", "9000"],
+    ];
+    for (let i = 0; i < table_data.length; i++) {
+        insertRowToDataTable(table_data[i]);
+    }
+}
 
 function insertRowToDataTable(data_array) {
     let table = document.querySelectorAll("tbody")[0];
