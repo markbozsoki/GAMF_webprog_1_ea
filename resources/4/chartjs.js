4ac57e6047fcc1741aa6ec10d517f9df748d1cdf
MoSzabina <moszabina@gmail.com>
2025-03-19 19:43:28 +0100

    Update resources/4/chartjs.js
    Co-authored-by: Márk Bozsóki <83731022+markbozsoki@users.noreply.github.com>

--- resources/4/chartjs.js
+++ resources/4/chartjs.js
@@ -3,7 +3,7 @@ const ctx = document.getElementById('chartCanvas').getContext('2d');
 let chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Érték 1', 'Érték 2', 'Érték 3', 'Érték 4', 'Érték 5'],
+        labels: [...document.getElementsByClassName("tableHeaders")],
         datasets: [{
             label: 'Kiválasztott sor',
             data: [],
