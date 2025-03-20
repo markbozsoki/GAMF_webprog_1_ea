c709d3e742cc993f3b3ba897ca913b8f35be7635
markbozsoki <bozsoki.mark@icloud.com>
2025-03-20 18:02:56 +0100

    fix_number_ordering_issue

--- resources/2/order_table.js
+++ resources/2/order_table.js
@@ -19,13 +19,18 @@ function sortTable(columnIndex, sort_asc) {
     let table_rows = document.querySelectorAll("tbody tr");
     let array_rows = [...table_rows]; // convert to array
     array_rows.sort((a, b) => {
-        let first_row = a.querySelectorAll("td")[columnIndex].textContent.toLowerCase();
-        let second_row = b.querySelectorAll("td")[columnIndex].textContent.toLowerCase();
+        let first_row_cell = a.querySelectorAll("td")[columnIndex].textContent.toLowerCase();
+        let second_row_cell = b.querySelectorAll("td")[columnIndex].textContent.toLowerCase();
+
+        if (columnIndex == 3) { // Points column index
+            first_row_cell = Number(first_row_cell);
+            second_row_cell = Number(second_row_cell);
+        }
 
         if (sort_asc) {
-            return first_row < second_row ? 1 : -1; // asc
+            return first_row_cell < second_row_cell ? 1 : -1; // asc
         } else {
-            return first_row < second_row ? -1 : 1; // desc
+            return first_row_cell < second_row_cell ? -1 : 1; // desc
         }
     }).map(sorted_row => document.querySelector("tbody").appendChild(sorted_row)); // apply sort on dataTable
 
