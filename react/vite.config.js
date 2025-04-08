8fcbd1573e8b56d6180576609441d53e49e27134
MoSzabina <moszabina@gmail.com>
2025-04-08 14:00:53 +0200

    changed_vite_entry_point

--- react/vite.config.js
+++ react/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //root: 'resources/react'
+  build: {
+    rollupOptions: {
+      input: {
+        app: 'react.html',
+      },
+    },
+  }, 
 })
