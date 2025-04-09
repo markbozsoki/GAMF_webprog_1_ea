9917fee1c791c06442ef9cd71d31f22fd078d84b
MoSzabina <moszabina@gmail.com>
2025-04-09 12:21:20 +0200

    hangman_components

---
+++ react/hangman/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <>
+            <h1>Akasztófa</h1>
+            <p>Találja ki a szót a billentyűzetről beírt betűkkel!</p>
+        </>
+    )
+}
+
+export default Header
